c4d27a11b1deaab2612f99b54771fc7759c45519
Philipp Chapkovski <chapkovski@gmail.com>
2023-09-18 15:21:27 +0200

    adding file logger

--- traderabbit/custom_logger.py
+++ traderabbit/custom_logger.py
@@ -2,7 +2,7 @@
 
 from termcolor import colored
 import logging
-
+import os
 
 class CustomFormatter(logging.Formatter):
     COLORS = {
@@ -18,15 +18,28 @@ class CustomFormatter(logging.Formatter):
         return colored(log_message, self.COLORS.get(record.levelname))
 
 
-def setup_custom_logger(name):
+def setup_custom_logger(name, log_directory="logs"):
+    # Create log directory if it doesn't exist
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
+    # Console Handler
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
 
+    # File Handler
+    fh = logging.FileHandler(os.path.join(log_directory, f"{name}.log"))
+    fh.setLevel(logging.DEBUG)
+
     formatter = CustomFormatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
     ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
 
     logger.addHandler(ch)
+    logger.addHandler(fh)
+
     return logger
