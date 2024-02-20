a533af08054f542de4bfc3492a68b5649e9b0361
Philipp Chapkovski <chapkovski@gmail.com>
2024-02-20 19:51:21 +0100

    adding connector to root

---
+++ client_connector/main.py
@@ -0,0 +1,108 @@
+import asyncio
+
+from fastapi import FastAPI, WebSocket, HTTPException, WebSocketDisconnect, BackgroundTasks
+
+from fastapi.middleware.cors import CORSMiddleware
+from client_connector.trader_manager import TraderManager
+from structures import TraderCreationData, TraderManagerParams
+import logging
+
+logger = logging.getLogger(__name__)
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# TODO: for now we keep it in memory, but we may want to persist it in a database
+trader_manager: TraderManager = None
+
+
+@app.get("/traders/defaults")
+async def get_trader_defaults():
+    # Get the schema of the model
+    schema = TraderCreationData.schema()
+
+    # Extract default values from the schema
+    defaults = {field: props.get("default") for field, props in schema.get("properties", {}).items() if
+                "default" in props}
+
+    return {
+        "status": "success",
+        "data": defaults
+    }
+
+
+@app.post("/trading/initiate")
+async def create_trading_session(params: TraderManagerParams, background_tasks: BackgroundTasks):
+    global trader_manager
+    trader_manager = TraderManager({
+        "n_noise_traders": params.n_noise_traders,
+        "n_human_traders": params.n_human_traders,
+        "activity_frequency": params.activity_frequency
+    })
+
+    background_tasks.add_task(trader_manager.launch)
+    return {
+        "status": "success",
+        "message": "New trader created",
+        "data": {"trader_uuid": trader_manager.human_trader.id}
+    }
+
+
+@app.websocket("/trader/{trader_uuid}")
+async def websocket_trader_endpoint(websocket: WebSocket, trader_uuid: str):
+    await websocket.accept()
+
+    global trader_manager
+    if trader_manager is None or not trader_manager.exists(trader_uuid):
+        await websocket.send_json({
+            "status": "error",
+            "message": "Trader not found",
+            "data": {}
+        })
+        await websocket.close()
+        return
+
+    trader = trader_manager.get_trader(trader_uuid)
+    trader.connect_to_socket(websocket)
+    logger.info(f"Trader {trader_uuid} connected to websocket")
+    # Send current status immediately upon new connection
+    await websocket.send_json({
+        "type": "success",
+        "message": "Connected to trader",
+        "data": {
+            "trader_uuid": trader_uuid,
+            "order_book": trader.order_book
+        }
+    })
+
+    try:
+        while True:
+            message = await websocket.receive_text()
+            await trader.on_message_from_client(message)
+    except WebSocketDisconnect:
+        await trader_manager.cleanup()  # This will now cancel all tasks
+    except asyncio.CancelledError:
+        logger.warning("Task cancelled")
+        await trader_manager.cleanup()  # This will now cancel all tasks
+
+
+@app.get("/traders/list")
+async def list_traders():
+    return {
+        "status": "success",
+        "message": "List of traders",
+        "data": {"traders": list(trader_manager.traders.keys())}
+    }
+
+
+@app.get("/")
+async def root():
+    return {"status": "trading is active",
+            "comment": "this is only for accessing trading platform mostly via websockets"}
+
+
---
+++ client_connector/trader_manager.py
@@ -0,0 +1,68 @@
+"""
+This file is for the trader manager: it is needed for connectingn, launching and managing human traders connections.
+Upon the request  of a client, it lauchnes new trading sessions and/or helps finding the exsiting traders and return their ids back to clients
+so they can communicate with them.
+"""
+
+import uuid
+
+from traders import HumanTrader, NoiseTrader
+from main_platform import TradingSession
+import logging
+import asyncio
+
+logger = logging.getLogger(__name__)
+
+
+class TraderManager:
+    trading_system: TradingSession = None
+    traders = {}
+    human_trader: HumanTrader = None
+
+    def __init__(self, params: dict):
+        self.tasks = []
+        n_noise_traders = params.get("n_noise_traders", 1)
+        # TODO: we may start launching with more than one human trader later.
+        # So far for debugging purposes we only need one human trader whose id we return to the client
+        n_human_traders = 1
+        self.human_trader = HumanTrader()
+        activity_frequency = params.get("activity_frequency", 5)
+        self.noise_traders = [NoiseTrader(activity_frequency=activity_frequency) for _ in range(n_noise_traders)]
+        self.human_traders = [self.human_trader]
+        self.traders = {t.id: t for t in self.noise_traders + self.human_traders}
+        self.trading_session = TradingSession()
+
+    async def launch(self):
+        await self.trading_session.initialize()
+        logger.info(f"Trading session UUID: {self.trading_session.id}")
+
+        for trader_id, trader in self.traders.items():
+            await trader.initialize()
+            await trader.connect_to_session(trading_session_uuid=self.trading_session.id)
+        for trader in self.noise_traders:
+            await trader.warm_up(starting_price=50, step=0.5, number_of_steps=10, number_of_warmup_orders=10)
+        await self.trading_session.send_broadcast({"content": "Market is open"})
+
+        trading_session_task = asyncio.create_task(self.trading_session.run())
+        trader_tasks = [asyncio.create_task(i.run()) for i in self.traders.values()]
+
+        self.tasks.append(trading_session_task)
+        self.tasks.extend(trader_tasks)
+
+        await trading_session_task
+
+    async def cleanup(self):
+        await self.trading_session.clean_up()
+        for trader in self.traders.values():
+            await trader.clean_up()
+        for task in self.tasks:
+            task.cancel()  # Request cancellation of the task
+        await asyncio.gather(*self.tasks, return_exceptions=True)
+        self.tasks.clear()  # Clear the list of tasks after cancellation
+
+
+    def get_trader(self, trader_uuid):
+        return self.traders.get(trader_uuid)
+
+    def exists(self, trader_uuid):
+        return trader_uuid in self.traders
