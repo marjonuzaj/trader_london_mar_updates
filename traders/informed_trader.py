22883392e32ec6326c762fb7bf2fe7d1575d50b7
dthinkr <venvoo@icloud.com>
2024-03-14 22:39:14 +0000

    corrected informed trader

--- traders/informed_trader.py
+++ traders/informed_trader.py
@@ -1,15 +1,30 @@
-from .base_trader import BaseTrader
 import asyncio
+import random
+from datetime import datetime
 from structures import OrderType, TraderType
 from main_platform.custom_logger import setup_custom_logger
-from datetime import datetime, timezone
 from main_platform.utils import now
+from .base_trader import BaseTrader
+
 logger = setup_custom_logger(__name__)
+
 
 class InformedTrader(BaseTrader):
 
-    def __init__(self, activity_frequency: int, settings: dict, settings_informed: dict, informed_state: dict, trading_day_duration: int,
-                 get_informed_time_plan: callable, get_signal_informed: callable, get_informed_order: callable):
+    def __init__(
+        self,
+        activity_frequency: int,
+        settings: dict,
+        settings_informed: dict,
+        informed_state: dict,
+        trading_day_duration: int,
+        get_informed_time_plan: callable,
+        get_signal_informed: callable,
+        get_informed_order: callable,
+    ):
+        """
+        initializes the informed trader with settings and callable functions.
+        """
         super().__init__(trader_type=TraderType.INFORMED)
         self.activity_frequency = activity_frequency
         self.settings = settings
@@ -18,70 +33,113 @@ class InformedTrader(BaseTrader):
         self.trading_day_duration = trading_day_duration
         self.get_informed_time_plan = get_informed_time_plan
         self.get_signal_informed = get_signal_informed
-        self.time_plan = self.get_informed_time_plan(self.settings_informed, self.settings)
-        self.step_frequency = self.trading_day_duration * 60 / (self.settings['n_updates_session'] + self.settings['warmup_periods'])
-        
-        """"TODO REQUIRES A GLOBAL REFACTORING OF STEP-TIME MECHANISM.
-                 MACHINE AGENTS CAN SHARE THE INTERNAL CLOCK OF THE TRADING PLATFORM
-        """
+        self.time_plan = self.get_informed_time_plan(
+            self.settings_informed, self.settings
+        )
+        self.step_frequency = (
+            self.trading_day_duration
+            * 60
+            / (self.settings["n_updates_session"] + self.settings["warmup_periods"])
+        )
+
         self.current_step = 0
-        
+
     @property
     def current_time(self) -> datetime:
+        """
+        a temporary solution to map time to step.
+        """
         return now()
-    
+
     def get_best_bid_and_ask(self) -> tuple:
+        """
+        a helper function to decide on price.
+        """
         best_bid = None
         best_ask = None
 
         for order in self.active_orders:
-            if order['order_type'] == 'bid':
-                if best_bid is None or order['price'] > best_bid['price']:
+            if order["order_type"] == "bid":
+                if best_bid is None or order["price"] > best_bid["price"]:
                     best_bid = order
-            elif order['order_type'] == 'ask':
-                if best_ask is None or order['price'] < best_ask['price']:
+            elif order["order_type"] == "ask":
+                if best_ask is None or order["price"] < best_ask["price"]:
                     best_ask = order
 
         return best_bid, best_ask
 
-    def get_informed_order(self, signal_informed: list, settings_informed: dict) -> dict:
+    def get_informed_order(
+        self, signal_informed: list, settings_informed: dict
+    ) -> dict:
+        """
+        generates informed trader's orders.
+        TODO
+        THE PRICE SHOULD BE LEARNED FROM PROFITS AND LOSSES.
+        """
         action, num_shares = signal_informed[0], signal_informed[1]
         best_bid, best_ask = self.get_best_bid_and_ask()
         order = {}
-        if action == 1: 
-            if settings_informed['inv'] > 0:
-                price = best_ask['price'] - 1 if best_ask else self.settings['initial_price']
-                order = {'bid': {price: [num_shares]}, 'ask': {}}
-            elif settings_informed['inv'] < 0:
-                price = best_bid['price'] + 1 if best_bid else self.settings['initial_price']
-                order = {'bid': {}, 'ask': {price: [num_shares]}}
+        price_variation = random.randint(1, 10)
+
+        if action == 1:
+            if settings_informed["inv"] > 0:
+                price = (
+                    (best_ask["price"] - price_variation)
+                    if best_ask
+                    else self.settings["initial_price"] - price_variation
+                )
+                order = {"bid": {price: [num_shares]}, "ask": {}}
+            elif settings_informed["inv"] < 0:
+                price = (
+                    (best_bid["price"] + price_variation)
+                    if best_bid
+                    else self.settings["initial_price"] + price_variation
+                )
+                order = {"bid": {}, "ask": {price: [num_shares]}}
 
         return order
 
     async def act(self):
-        signal = self.get_signal_informed(self.informed_state, self.settings_informed, self.current_step)
+        """
+        loads step-wise signal and generates orders.
+        """
+        signal = self.get_signal_informed(
+            self.informed_state, self.settings_informed, self.current_step
+        )
         order_dict = self.get_informed_order(signal, self.settings_informed)
 
         for order_type, orders in order_dict.items():
             for price, amounts in orders.items():
                 for amount in amounts:
-                    msg = f"""POSTED {order_type} AT {price} AMOUNT {amount} 
-                        AT STEP {self.current_step} AT TIME {self.current_time.strftime('%H:%M:%S')}"""
-                    if order_type == 'bid':
+                    if order_type == "bid":
                         await self.post_new_order(amount, price, OrderType.BID)
-                    elif order_type == 'ask':
+                    elif order_type == "ask":
                         await self.post_new_order(amount, price, OrderType.ASK)
+                    logger.info(
+                        "POSTED %s AT %s AMOUNT %s AT STEP %s AT TIME %s",
+                        order_type,
+                        price,
+                        amount,
+                        self.current_step,
+                        self.current_time.strftime("%H:%M:%S"),
+                    )
 
     async def run(self):
+        """
+        trades at each step.
+        """
         while not self._stop_requested.is_set():
             try:
                 await self.act()
                 await asyncio.sleep(self.step_frequency)
                 self.current_step += 1
             except asyncio.CancelledError:
-                logger.info('Run method cancelled, performing cleanup of informed trader...')
+                logger.info(
+                    "Run method cancelled, performing cleanup of %s...",
+                    self.trader_type,
+                )
                 await self.clean_up()
                 raise
             except Exception as e:
-                logger.error(f"An error occurred in InformedTrader run loop: {e}")
+                logger.error("An error occurred in InformedTrader run loop: %s", e)
                 break
--- traders/noise_trader.py
+++ traders/noise_trader.py
@@ -1,18 +1,31 @@
-from .base_trader import BaseTrader
-from main_platform.utils import convert_to_book_format, convert_to_noise_state, convert_to_trader_actions
 import asyncio
 import random
-from structures import OrderType, TraderType, ORDER_AMOUNT
 import numpy as np
-
+from structures import OrderType, TraderType, ORDER_AMOUNT
+from main_platform.utils import (
+    convert_to_book_format,
+    convert_to_noise_state,
+    convert_to_trader_actions,
+)
 from main_platform.custom_logger import setup_custom_logger
+from .base_trader import BaseTrader
 
 logger = setup_custom_logger(__name__)
 
+
 class NoiseTrader(BaseTrader):
 
-    def __init__(self, activity_frequency: float, settings: dict, settings_noise: dict,
-                 get_signal_noise: callable, get_noise_rule_unif: callable):
+    def __init__(
+        self,
+        activity_frequency: float,
+        settings: dict,
+        settings_noise: dict,
+        get_signal_noise: callable,
+        get_noise_rule_unif: callable,
+    ):
+        """
+        initializes the noise trader with settings and callable functions.
+        """
         super().__init__(trader_type=TraderType.NOISE)
 
         self.activity_frequency = activity_frequency
@@ -22,91 +35,123 @@ class NoiseTrader(BaseTrader):
         self.get_noise_rule_unif = get_noise_rule_unif
         self.current_variance = 5.0
 
-    def cooling_interval(self, target: float, initial_variance: float = 5.0, 
-                         decay_rate: float = 0.9) -> float:
+    def cooling_interval(
+        self, target: float, initial_variance: float = 5.0, decay_rate: float = 0.9
+    ) -> float:
+        """
+        adjusts cooling interval using a random process.
+        """
         if self.current_variance is None:
             self.current_variance = initial_variance
         interval = random.gauss(target, np.sqrt(self.current_variance))
-        self.current_variance *= decay_rate  # Update the current variance
+        self.current_variance *= decay_rate
         return interval
-    
+
     async def act(self):
+        """
+        generates action based on active orders in the market.
+        """
         if not self.active_orders:
-            await self.post_new_order(ORDER_AMOUNT, self.settings['initial_price'], OrderType.ASK)
+            await self.post_new_order(
+                ORDER_AMOUNT,
+                self.settings["initial_price"],
+                random.choice([OrderType.ASK, OrderType.BID]),
+            )
             return
 
         book_format = convert_to_book_format(self.active_orders)
         noise_state = convert_to_noise_state(self.orders)
-        signal_noise = self.get_signal_noise(signal_state=None, settings_noise=self.settings_noise)
-        noise_orders = self.get_noise_rule_unif(book_format, signal_noise, noise_state, self.settings_noise, self.settings)
+        signal_noise = self.get_signal_noise(
+            signal_state=None, settings_noise=self.settings_noise
+        )
+        noise_orders = self.get_noise_rule_unif(
+            book_format, signal_noise, noise_state, self.settings_noise, self.settings
+        )
         orders = convert_to_trader_actions(noise_orders)
 
         bid_count, ask_count = 0, 0
         for order in self.active_orders:
-            if order['order_type'] == 'bid':
+            if order["order_type"] == "bid":
                 bid_count += 1
-            elif order['order_type'] == 'ask':
+            elif order["order_type"] == "ask":
                 ask_count += 1
-                
+
         order_type_override = None
+        order_type = None
+
         if bid_count == 0 and ask_count > 0:
-            logger.critical("No bids in the market, placing bid to balance.")
+            order_type = OrderType.BID
             order_type_override = OrderType.BID
         elif ask_count == 0 and bid_count > 0:
-            logger.critical("No asks in the market, placing ask to balance.")
+            order_type = OrderType.ASK
             order_type_override = OrderType.ASK
+
+        logger.info(
+            "NO %s IN THE MARKET, PUTTING %s TO BALANCE",
+            order_type,
+            order_type_override,
+        )
 
         for order in orders:
             if order_type_override is not None:
-                order['order_type'] = 'ask' if order_type_override == OrderType.ASK else 'bid'
+                order["order_type"] = (
+                    "ask" if order_type_override == OrderType.ASK else "bid"
+                )
 
             await self.process_order(order)
 
-    async def process_order(self, order, book_format=None):
-        if order['order_type'] == 'ask' or order['order_type'] == 'bid':
-            order['price'] = self.adjust_order_price(order['order_type'], book_format, order['price'])
-
-        if order['action_type'] == 'add_order':
-            order_type = OrderType.ASK if order['order_type'] == 'ask' else OrderType.BID
-            amount, price = ORDER_AMOUNT, order['price']
-            for _ in range(order['amount']):
+    async def process_order(self, order):
+        if order["action_type"] == "add_order":
+            order_type = (
+                OrderType.ASK if order["order_type"] == "ask" else OrderType.BID
+            )
+            amount, price = ORDER_AMOUNT, order["price"]
+            for _ in range(order["amount"]):
                 await self.post_new_order(amount, price, order_type)
-            logger.info(f"POSTED {order['order_type']} AT {price} AMOUNT {ORDER_AMOUNT} * {order['amount']}")
-        elif order['action_type'] == 'cancel_order' and self.orders:
-            matching_orders = [o for o in self.orders if o['order_type'] == order['order_type']]
-            if matching_orders:
-                order_id = random.choice(matching_orders)['id']
-                await self.send_cancel_order_request(order_id)
-                logger.info(f"CANCELLED {order['order_type']} ID {order_id[:10]}")
 
-    def adjust_order_price(self, order_type, book_format, original_price):
-        """ADJUSTS ORDER PRICE BASED ON MARKET CONDITION"""
-        if order_type == OrderType.BID:
-            highest_bid = max(book_format['bids'], key=lambda x: x['price'], default={'price': original_price})['price']
-            new_price = highest_bid + self.settings['price_increment']
-        elif order_type == OrderType.ASK:
-            lowest_ask = min(book_format['asks'], key=lambda x: x['price'], default={'price': original_price})['price']
-            new_price = lowest_ask - self.settings['price_increment']
-        else:
-            new_price = original_price
-        return new_price
+            logger.info(
+                "POSTED %s AT %s AMOUNT %s * %s",
+                order["order_type"],
+                price,
+                ORDER_AMOUNT,
+                order["amount"],
+            )
+
+        elif order["action_type"] == "cancel_order" and self.orders:
+            matching_orders = [
+                o for o in self.orders if o["order_type"] == order["order_type"]
+            ]
+            if matching_orders:
+                order_id = random.choice(matching_orders)["id"]
+                await self.send_cancel_order_request(order_id)
+                logger.info("CANCELLED %s ID %s", order["order_type"], order_id[:10])
 
     async def warm_up(self, number_of_warmup_orders: int):
+        """
+        places warmup orders to poulate order book.
+        """
         for _ in range(number_of_warmup_orders):
-            print(f' WARMING UP {_} ')
+            print(f" WARMING UP {_} ")
             await self.act()
 
     async def run(self):
+        """
+        trades at cooling intervals.
+        """
         while not self._stop_requested.is_set():
             try:
                 await self.act()
 
-                await asyncio.sleep(self.cooling_interval(target=self.activity_frequency))
-             
+                await asyncio.sleep(
+                    self.cooling_interval(target=self.activity_frequency)
+                )
+
             except asyncio.CancelledError:
-                logger.info('Run method cancelled, performing cleanup of noise trader...')
+                logger.info(
+                    "Run method cancelled, performing cleanup of %s...", self.trader_type
+                )
                 await self.clean_up()
                 raise
             except Exception as e:
-                logger.error(f"An error occurred in NoiseTrader run loop: {e}")
+                logger.error("An error occurred in NoiseTrader run loop: %s", e)
                 break
